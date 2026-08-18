2388814c2032febd8e893e71e34ce18b5143dae4
shamikhstudio-gif <shamikhstudio@gmail.com>
2026-08-18 22:44:52 +0300

    feat(security): apply spy account defense (16 Aug 2026 cutoff) and executive appeal system [src/vcr/config.js]

--- src/vcr/config.js
+++ src/vcr/config.js
@@ -1,9 +1,39 @@
 export const VCR_CONFIGS = [
-  { id: '1539231767683137646', name: 'GX VCR #1', token: 'MTUzOTIzMTc2NzY4MzEzNzY0Ng.Gkf3Fx.vGNWnJPkznujkNruXkVRgO59S6Azm_GtCYozwM' },
-  { id: '1539241189629362246', name: 'GX VCR #2', token: 'MTUzOTI0MTE4OTYyOTM2MjI0Ng.GD4es9.op6hFAccGcCdxk3rNKVwzp9kYHQwFomH79LIUM' },
-  { id: '1539241414318227466', name: 'GX VCR #3', token: 'MTUzOTI0MTQxNDMxODIyNzQ2Ng.GX1bC4.PuKwmdSFkPqbNOSdBglS6MSWcnIPqTK1NMAMyM' },
-  { id: '1539241621328101497', name: 'GX VCR #4', token: 'MTUzOTI0MTYyMTMyODEwMTQ5Nw.GI0Upa.B39f0NNxKaBmcT3plb4Pwf-C8amlPLkkNDB7rQ' },
-  { id: '1539241867105927209', name: 'GX VCR #5', token: 'MTUzOTI0MTg2NzEwNTkyNzIwOQ.Gm5orN.ihvx7BGFF_JF5bIREsnS4qE9WkNA4t8Rl6Ox4w' }
+  { 
+    id: '1539231767683137646', 
+    name: 'GX VCR #1', 
+    token: 'MTUzOTIzMTc2NzY4MzEzNzY0Ng.Gkf3Fx.vGNWnJPkznujkNruXkVRgO59S6Azm_GtCYozwM',
+    defaultChannelId: '1537461175908958259',
+    defaultChannelName: '『🔊』・𝑽𝒐𝒊𝒄𝒆-𝟎𝟏'
+  },
+  { 
+    id: '1539241189629362246', 
+    name: 'GX VCR #2', 
+    token: 'MTUzOTI0MTE4OTYyOTM2MjI0Ng.GD4es9.op6hFAccGcCdxk3rNKVwzp9kYHQwFomH79LIUM',
+    defaultChannelId: '1538568733692530798',
+    defaultChannelName: '🔒・فويس الإدارة'
+  },
+  { 
+    id: '1539241414318227466', 
+    name: 'GX VCR #3', 
+    token: 'MTUzOTI0MTQxNDMxODIyNzQ2Ng.GX1bC4.PuKwmdSFkPqbNOSdBglS6MSWcnIPqTK1NMAMyM',
+    defaultChannelId: '1538564154141577276',
+    defaultChannelName: '『🔊』・𝑽𝒐𝒊𝒄𝒆-𝟎𝟐'
+  },
+  { 
+    id: '1539241621328101497', 
+    name: 'GX VCR #4', 
+    token: 'MTUzOTI0MTYyMTMyODEwMTQ5Nw.GI0Upa.B39f0NNxKaBmcT3plb4Pwf-C8amlPLkkNDB7rQ',
+    defaultChannelId: '1538564786680233984',
+    defaultChannelName: '『🔊』・𝑽𝒐𝒊𝒄𝒆-𝟎𝟑'
+  },
+  { 
+    id: '1539241867105927209', 
+    name: 'GX VCR #5', 
+    token: 'MTUzOTI0MTg2NzEwNTkyNzIwOQ.Gm5orN.ihvx7BGFF_JF5bIREsnS4qE9WkNA4t8Rl6Ox4w',
+    defaultChannelId: '1538564321905479731',
+    defaultChannelName: '『🔊』・𝑽𝒐𝒊𝒄𝒆-𝟎𝟒'
+  }
 ];
 
 export const VCR_ROLE_NAME = '🎙️ GX VCR';
